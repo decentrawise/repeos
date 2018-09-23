c842306f094f10219decec4ccb0e1ab0878d7cd5
mtm <mtadeunet@gmail.com>
2018-09-23 02:16:45 +0100

    make docker install repeos instead of note example

--- eosio_docker/scripts/init_blockchain.sh
+++ eosio_docker/scripts/init_blockchain.sh
@@ -37,16 +37,17 @@ cleos wallet import -n eosiomain --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4
 
 echo "=== setup wallet: notechainwal ==="
 # key for eosio account and export the generated password to a file for unlocking wallet later
-cleos wallet create -n notechainwal --to-console | tail -1 | sed -e 's/^"//' -e 's/"$//' > notechain_wallet_password.txt
+cleos wallet create -n repeoswal --to-console | tail -1 | sed -e 's/^"//' -e 's/"$//' > notechain_wallet_password.txt
 # Owner key for notechainwal wallet
-cleos wallet import -n notechainwal --private-key 5JpWT4ehouB2FF9aCfdfnZ5AwbQbTtHBAwebRXt94FmjyhXwL4K
+cleos wallet import -n repeoswal --private-key 5JpWT4ehouB2FF9aCfdfnZ5AwbQbTtHBAwebRXt94FmjyhXwL4K
 # Active key for notechainwal wallet
-cleos wallet import -n notechainwal --private-key 5JD9AGTuTeD5BXZwGQ5AtwBqHK21aHmYnTetHgk1B3pjj7krT8N
+cleos wallet import -n repeoswal --private-key 5JD9AGTuTeD5BXZwGQ5AtwBqHK21aHmYnTetHgk1B3pjj7krT8N
 
 # * Replace "notechainwal" by your own wallet name when you start your own project
 
 # create account for notechainacc with above wallet's public keys
-cleos create account eosio notechainacc EOS6PUh9rs7eddJNzqgqDx1QrspSHLRxLMcRdwHZZRL4tpbtvia5B EOS8BCgapgYA2L4LJfCzekzeSr3rzgSTUXRXwNi8bNRoz31D14en9
+#cleos create account eosio notechainacc EOS6PUh9rs7eddJNzqgqDx1QrspSHLRxLMcRdwHZZRL4tpbtvia5B EOS8BCgapgYA2L4LJfCzekzeSr3rzgSTUXRXwNi8bNRoz31D14en9
+cleos create account eosio repeosacc EOS6PUh9rs7eddJNzqgqDx1QrspSHLRxLMcRdwHZZRL4tpbtvia5B EOS8BCgapgYA2L4LJfCzekzeSr3rzgSTUXRXwNi8bNRoz31D14en9
 
 # * Replace "notechainacc" by your own account name when you start your own project
 
@@ -55,7 +56,7 @@ echo "=== deploy smart contract ==="
 # $2 account holder name of the smart contract
 # $3 wallet for unlock the account
 # $4 password for unlocking the wallet
-deploy_contract.sh notechain notechainacc notechainwal $(cat notechain_wallet_password.txt)
+deploy_contract.sh repeos repeosacc repeoswal $(cat notechain_wallet_password.txt)
 
 echo "=== create user accounts ==="
 # script for create data into blockchain
